ac577e204c7be44eb605d15552a0c0a7770e67ad
Ayebakuro <xplicitkuro@gmail.com>
2018-11-06 18:51:07 +0100

    New Dev Branch

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+
+//set up express app
+const app = express();
+
+
+app.use(express.static('public'));
+
+app.use('/', routes);
+
+// listen for request
+app.listen(process.env.port || 4000, function(){
+    console.log('now listening for request');
+});
